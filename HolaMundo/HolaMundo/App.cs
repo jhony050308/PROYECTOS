9d9ca546f35f2fef2c84d36cda4091862de7df6d
jhony050308 <jonathan.olvera@comunidad.unam.mx>
2015-02-09 23:34:08 -0600

    ActHolaMundo

---
+++ HolaMundo/HolaMundo/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HolaMundo
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new ContentPage { 
+				Content = new Label {
+					Text = "Jonathan Israel Olvera Ramirez,412097843",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				},
+			};
+		}
+	}
+}
+
