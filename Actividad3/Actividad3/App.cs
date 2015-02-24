9ba9f497bd1c52d48afcb904600a9a5470d5bcb4
jhony050308 <jonathan.olvera@comunidad.unam.mx>
2015-02-24 17:50:49 -0600

    Actividad3

---
+++ Actividad3/Actividad3/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Actividad3
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new ContentPage { 
+				Content = new Label {
+					Text = "Hello, Forms!",
+					VerticalOptions = LayoutOptions.EndAndExpand,
+					HorizontalOptions = LayoutOptions.StartAndExpand,
+					BackgroundColor = Color.Green,
+					TextColor = Color.Yellow,
+
+				},
+			};
+		}
+	}
+}
+
