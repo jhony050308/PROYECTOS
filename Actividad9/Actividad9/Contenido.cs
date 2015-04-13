be9b729533f73b107b2c53dc549e0c2d004faebf
jhony050308 <jonathan.olvera@comunidad.unam.mx>
2015-04-12 23:41:33 -0500

    Actividad 9

---
+++ Actividad9/Actividad9/Contenido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Actividad9
+{
+	public class Contenido: ContentPage
+	{
+		public Contenido ()
+		{
+			Label texto = new Label {
+				Text = "Actividad 9",
+				TextColor = Color.Green
+			};
+
+			StackLayout stackLayout = new StackLayout 
+			{
+				Children = 
+				{
+					texto
+
+				}
+				};
+
+			this.Content = stackLayout;
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
+		}
+	}
+}
+
+
+
